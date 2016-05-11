8f28f89307e4c17e868d533707985bf1d2587646
Ross McIlwaine <rdm0423@gmail.com>
2016-05-11 10:09:48 -0600

    Adds Stretch Problem Solution

--- StretchProblem-Persons.playground/Contents.swift
+++ StretchProblem-Persons.playground/Contents.swift
@@ -5,7 +5,7 @@ import UIKit
 var str = "Hello, playground"
 
 
-class Person {
+class Person: Equatable {
     
     let firstName: String
     let lastName: String
@@ -18,17 +18,27 @@ class Person {
     }
 }
 
+func ==(lhs: Person, rhs: Person) -> Bool {
+    
+    return lhs.age == rhs.age && lhs.firstName == rhs.firstName && lhs.lastName == rhs.lastName
+}
+
 let james = Person(firstName: "James", lastName: "Pacheo", age: 26)
 let andrea = Person(firstName: "Andrea", lastName: "Mower", age: 24)
 let rebecca = Person(firstName: "Rebecca", lastName: "Mordo", age: 30)
 let caleb = Person(firstName: "Caleb", lastName: "Hicks", age: 36)
 let ryan = Person(firstName: "Ryan", lastName: "Watt", age: 24)
-let clubMembers: [Person] = [james, andrea, rebecca, caleb]
+var clubMembers: [Person] = [james, andrea, rebecca]
+clubMembers.append(caleb)
+
 
 func areYouAMemberOfTheClub(person: Person) -> Bool {
-    var isMember: Bool = true
     
-    if 
+    return clubMembers.contains(person)
 }
+
+areYouAMemberOfTheClub(james)
+areYouAMemberOfTheClub(ryan)
+
 
 
